fa8e1495fa25557672c8dfc65345b5b9b500fe43
David Banks <amoebae@gmail.com>
2011-06-14 16:28:24 +0100

    add check for missing data

--- quake.sh
+++ quake.sh
@@ -5,4 +5,10 @@
 data_location=/usr/share/games/quake
 engine_path=/usr/games/quakespasm
 
+no_data_msg="Missing data; see /usr/share/doc/quake/README.Debian"
+
+if ! [ -f "${data_location}/id1/pak0.pak" ]; then
+    exec "$data_location"/need-data.sh "$no_data_msg"
+fi
+
 exec ${engine_path} -basedir ${data_location}
