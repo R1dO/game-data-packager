0b37eda964c8c54edc240c95ed70896fa583026e
Simon McVittie <smcv@debian.org>
2011-07-06 23:20:10 +0100

    Define and use alternatives for quake-engine
    Also define quake-engine-server, but do not use it, yet.
    
    Also specifically depend on quake-registered | quake-shareware, not
    on quake-data.

--- quake.sh
+++ quake.sh
@@ -3,7 +3,7 @@
 # quake.sh - launcher script for quake 1
 
 data_location=/usr/share/games/quake
-engine_path=/usr/games/quakespasm
+engine_path=/usr/lib/quake/quake-engine
 no_data_msg="Missing data; see /usr/share/doc/quake/README.Debian"
 
 main() {
