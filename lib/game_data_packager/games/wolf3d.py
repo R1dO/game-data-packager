8a58a41bde24c22ba6225c3b5700c3daafa120c7
Alexandre Detiste <alexandre.detiste@gmail.com>
2015-01-12 10:56:25 +0100

    conversion of wolf3-demo-data to yaml

---
+++ lib/game_data_packager/games/wolf3d.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# vim:set fenc=utf-8:
+#
+# Copyright © 2014 Simon McVittie <smcv@debian.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You can find the GPL license text on a Debian system under
+# /usr/share/common-licenses/GPL-2.
+
+import logging
+
+from .. import GameData
+
+logger = logging.getLogger('game-data-packager.games.wolf3d')
+
+class Wolf3DGameData(GameData):
+    def add_parser(self, parsers):
+        parser = super(Wolf3DGameData, self).add_parser(parsers)
+        parser.add_argument('-f', action='append', dest='paths',
+                metavar='1wolf14.zip', help='Path to 1wolf14.zip')
+        parser.add_argument('-w', dest='download', action='store_true',
+                help='Download 1wolf14.zip (done automatically if necessary)')
+        return parser
+
+GAME_DATA_SUBCLASS = Wolf3DGameData
