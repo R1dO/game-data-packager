d9748cda4957c306e9bbd784ae01c90047823031
Alexandre Detiste <alexandre.detiste@gmail.com>
2015-07-19 15:08:27 +0200

    Grim Fandango: add Scythe icon, own work

--- game_data_packager/games/residualvm_common.py
+++ game_data_packager/games/residualvm_common.py
@@ -16,14 +16,19 @@
 # You can find the GPL license text on a Debian system under
 # /usr/share/common-licenses/GPL-2.
 
+import configparser
 import logging
 import os
+import subprocess
 
 from .. import GameData
 from ..paths import DATADIR
-from ..util import (TemporaryUmask, copy_with_substitutions, mkdir_p)
+from ..util import mkdir_p
 
 logger = logging.getLogger('game-data-packager.games.residualvm-common')
+
+def install_data(from_, to):
+    subprocess.check_call(['cp', '--reflink=auto', from_, to])
 
 class ResidualvmGameData(GameData):
     def __init__(self, shortname, data, workdir=None):
@@ -44,24 +49,51 @@ class ResidualvmGameData(GameData):
         if package.type == 'expansion':
             return
 
-        with TemporaryUmask(0o022):
-            appdir = os.path.join(destdir, 'usr/share/applications')
-            mkdir_p(appdir)
-            from_ = os.path.join(DATADIR, 'residualvm-common.desktop.in')
-            copy_with_substitutions(open(from_,
-                    encoding='utf-8'),
-                        open(os.path.join(appdir, '%s.desktop' % package.name),
-                            'w', encoding='utf-8'),
-                        GAME=self.gameid,
-                        PATH=package.install_to,
-                        LONG=(package.longname or self.longname))
+        icon = package.name
+        for from_ in (self.locate_steam_icon(package),
+                      os.path.join(DATADIR, package.name + '.png'),
+                      os.path.join(DATADIR, self.shortname + '.png'),
+                      os.path.join('/usr/share/pixmaps', icon + '.png'),
+                      os.path.join(DATADIR, self.shortname.strip('1234567890') + '.png')):
+            if from_ and os.path.exists(from_):
+                pixdir = os.path.join(destdir, 'usr/share/pixmaps')
+                mkdir_p(pixdir)
+                install_data(from_, os.path.join(pixdir, '%s.png' % icon))
+                break
+        else:
+            icon = 'residualvm'
 
-            lintiandir = os.path.join(destdir, 'usr/share/lintian/overrides')
-            mkdir_p(lintiandir)
-            with open(os.path.join(lintiandir, package.name),
-                      'a', encoding='utf-8') as o:
-                 o.write('%s: desktop-command-not-in-package '
-                         'usr/share/applications/%s.desktop residualvm\n'
-                         % (package.name, package.name))
+        from_ = os.path.splitext(from_)[0] + '.svgz'
+        if os.path.exists(from_):
+            svgdir = os.path.join(destdir, 'usr/share/icons/hicolor/scalable/apps')
+            mkdir_p(svgdir)
+            install_data(from_, os.path.join(svgdir, '%s.svgz' % icon))
+
+        appdir = os.path.join(destdir, 'usr/share/applications')
+        mkdir_p(appdir)
+
+        desktop = configparser.RawConfigParser()
+        desktop.optionxform = lambda option: option
+        desktop['Desktop Entry'] = {}
+        entry = desktop['Desktop Entry']
+        entry['Name'] = package.longname or self.longname
+        entry['GenericName'] = self.genre + ' Game'
+        entry['TryExec'] = 'residualvm'
+        entry['Icon'] = icon
+        entry['Terminal'] = 'false'
+        entry['Type'] = 'Application'
+        entry['Categories'] = 'Game'
+        entry['Exec'] = 'residualvm -p /%s %s' % (package.install_to, self.gameid)
+        with open(os.path.join(appdir, '%s.desktop' % package.name),
+                  'w', encoding='utf-8') as output:
+             desktop.write(output, space_around_delimiters=False)
+
+        lintiandir = os.path.join(destdir, 'usr/share/lintian/overrides')
+        mkdir_p(lintiandir)
+        with open(os.path.join(lintiandir, package.name),
+                  'a', encoding='utf-8') as o:
+             o.write('%s: desktop-command-not-in-package '
+                     'usr/share/applications/%s.desktop residualvm\n'
+                     % (package.name, package.name))
 
 GAME_DATA_SUBCLASS = ResidualvmGameData
