55f3e8885b51808aceeeadad0888d1e8d4da7510
Simon McVittie <smcv@debian.org>
2015-10-04 21:32:11 +0100

    grimfandango: use ?variation syntax

--- game_data_packager/__init__.py
+++ game_data_packager/__init__.py
@@ -826,7 +826,7 @@ class GameData(object):
                 assert wanted.size is not None or filename in (
                    'hipnotic/pak0.pak_qdq_glquake_compat',
                    'resource.1?106_cd',
-                   'vox0000.lab_unpatched',
+                   'vox0000.lab?unpatched',
                    ), (self.shortname, wanted.name)
 
     def construct_task(self, **kwargs):
