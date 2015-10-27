53e6bc40206214a125e840fae24905c6ca6d2c35
Alexandre Detiste <alexandre.detiste@gmail.com>
2015-10-27 10:23:27 +0100

    RTCW: fix French & Spanish mission 1 briefing (#803128)

---
+++ game_data_packager/games/rtcw.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# encoding=utf-8
+#
+# Copyright © 2015 Alexandre Detiste <alexandre@detiste.be>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 2
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You can find the GPL license text on a Debian system under
+# /usr/share/common-licenses/GPL-2.
+
+import logging
+import os
+import zipfile
+
+from .. import (GameData)
+from ..build import (PackagingTask, mkdir_p)
+
+logger = logging.getLogger('game-data-packager.games.rtcw')
+
+class RTCWGameData(GameData):
+    def construct_task(self, **kwargs):
+        return RTCWTask(self, **kwargs)
+
+class RTCWTask(PackagingTask):
+    def fill_dest_dir(self, package, destdir):
+        if not super(RTCWTask, self).fill_dest_dir(package, destdir):
+            return False
+
+        if package.name not in ('rtcw-es-data', 'rtcw-fr-data'):
+            return True
+
+        zip_in = os.path.join(destdir, 'usr/share/games/rtcw/main', 'sp_pak1.pk3')
+        libdir = os.path.join(destdir, 'usr/lib/rtcw/main')
+        mkdir_p(libdir)
+        zip_out = os.path.join(libdir, 'sp_pak5.pk3')
+
+        with zipfile.ZipFile(zip_in, 'r') as zf_in:
+            with zipfile.ZipFile(zip_out, 'w') as zf_out:
+                txt = zf_in.open('text/EnglishUSA/escape1.txt').read()
+                zf_out.writestr('text/EnglishUSA/escape1.txt', txt)
+
+        return True
+
+GAME_DATA_SUBCLASS = RTCWGameData
