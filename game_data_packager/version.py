3ef741f952ba6b81177b39fa4e91e5a745009939
Simon McVittie <smcv@debian.org>
2016-01-04 22:44:17 +0000

    version: detect features, not distributions, to load ...VERSION

--- game_data_packager/version.py
+++ game_data_packager/version.py
@@ -5,16 +5,21 @@
 
 import os
 
+with open('debian/changelog', encoding='utf-8') as cl:
+    try:
+        from debian.changelog import ChangeLog
+    except ImportError:
+        GAME_PACKAGE_VERSION = cl.readline().split('(', 1)[1].split(')', 1)[0]
+    else:
+        cl = ChangeLog(cl, strict=False)
+        GAME_PACKAGE_VERSION = str(cl.full_version)
+
 if os.path.isfile('/etc/debian_version'):
     FORMAT = 'deb'
     DISTRO = 'generic'
     BINDIR = 'usr/games'
     ASSETS = 'usr/share/games'
     LICENSEDIR = 'usr/share/doc'
-
-    from debian.changelog import Changelog
-    cl = Changelog(open('debian/changelog', encoding='utf-8'), strict=False)
-    GAME_PACKAGE_VERSION = str(cl.full_version)
 
 elif os.path.isfile('/etc/redhat-release'):
     FORMAT = 'rpm'
@@ -23,9 +28,6 @@ elif os.path.isfile('/etc/redhat-release'):
     ASSETS = 'usr/share'
     LICENSEDIR = 'usr/share/licenses'
 
-    cl = open('debian/changelog', encoding='utf-8').readline()
-    GAME_PACKAGE_VERSION = cl.split('(')[1].split(')')[0]
-
 elif os.path.isfile('/etc/SuSE-release'):
     FORMAT = 'rpm'
     DISTRO = 'suse'
@@ -33,15 +35,9 @@ elif os.path.isfile('/etc/SuSE-release'):
     ASSETS = 'usr/share'
     LICENSEDIR = 'usr/share/licenses'
 
-    cl = open('debian/changelog', encoding='utf-8').readline()
-    GAME_PACKAGE_VERSION = cl.split('(')[1].split(')')[0]
-
 elif os.path.isfile('/etc/arch-release'):
     FORMAT = 'arch'
     DISTRO = 'arch'
     BINDIR = 'usr/bin'
     ASSETS = 'usr/share'
     LICENSEDIR = 'usr/share/doc'
-
-    cl = open('debian/changelog', encoding='utf-8').readline()
-    GAME_PACKAGE_VERSION = cl.split('(')[1].split(')')[0]
