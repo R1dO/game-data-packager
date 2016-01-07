02e2e5ad2267865ba9eba9d58486a41715a24901
Simon McVittie <smcv@debian.org>
2016-01-07 11:38:55 +0000

    umod test: fix mode, fix interpreter, add copyright

--- tests/umod.py
+++ tests/umod.py
@@ -1,4 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+# encoding=utf-8
+#
+# Copyright © 2015 Simon McVittie <smcv@debian.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You can find the GPL license text on a Debian system under
+# /usr/share/common-licenses/GPL-2.
 
 import io
 import struct
