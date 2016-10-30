R1dO/game-data-packager

commit 0b022b5bcdecec5156f3896ea7f1e4708162169f

Use absolute paths for $assets, $bindir etc.
This makes the substitutions into the .desktop files a lot less odd.

game_data_packager/make_template.py
@@ -349,7 +349,7 @@ class GameData(object):
                 if os.path.isdir(path):
                     continue
                 elif os.path.islink(path):
-                    self.package.setdefault('symlinks', {})[name] = os.path.realpath(path).lstrip('/')
+                    self.package.setdefault('symlinks', {})[name] = os.path.realpath(path)
                 elif os.path.isfile(path):
                     self.add_file(path, out_name=out_name, lang=lang)
                 else:
@@ -359,7 +359,7 @@ class GameData(object):
                 logger.warning('DOSBOX files detected, make sure not to include those in your package')
 
         if self.plugin != 'scummvm_common':
-            self.package['install_to'] = 'usr/share/games/' + game
+            self.package['install_to'] = '$assets/' + game
 
         self.package['install'].sort()
         if self.package['optional']:
@@ -552,7 +552,8 @@ class GameData(object):
                             name, entry.type)
 
         if self.plugin != 'scummvm_common':
-            self.package['install_to'] = install_to
+            self.package['install_to'] = os.path.join('/',
+                    install_to).replace('/usr/share/games/', '$assets/')
         self.package['install'].sort()
         if self.package['optional']:
             self.package['optional'].sort()

game_data_packager/build.py
@@ -1148,7 +1148,7 @@ class PackagingTask(object):
         return complete
 
     def fill_docs(self, package, destdir, pkgdocdir):
-        copy_to = os.path.join(destdir, pkgdocdir, 'copyright')
+        copy_to = os.path.join(destdir, pkgdocdir.strip('/'), 'copyright')
         for n in (package.name, self.game.shortname):
             copy_from = os.path.join(DATADIR, n + '.copyright')
             if os.path.exists(copy_from):
@@ -1175,7 +1175,7 @@ class PackagingTask(object):
                  if not f.license:
                      continue
                  license_file = f.install_as
-                 licenses.add("/%s/%s/%s" % (self.packaging.LICENSEDIR,
+                 licenses.add(os.path.join('/', self.packaging.LICENSEDIR,
                      package.name, license_file))
                  if os.path.splitext(license_file)[0].lower() == 'license':
                      self.packaging.override_lintian(destdir, package.name,
@@ -1232,12 +1232,12 @@ class PackagingTask(object):
                 o.write('"/usr/share/doc/%s/%s"\n' % (package.name,
                                                       package.only_file))
             elif count_usr == 1:
-                o.write('"/%s/%s"\n' % (install_to, main_wad))
+                o.write('"%s"\n' % os.path.join('/', install_to, main_wad))
             elif len(exts) == 1:
-                o.write('The %s files under "/%s/"\n' %
-                        (list(exts)[0], install_to))
+                o.write('The %s files under "%s/"\n' %
+                        (list(exts)[0], os.path.join('/', install_to)))
             else:
-                o.write('The files under "/%s/"\n' % install_to)
+                o.write('The files under "%s/"\n' % os.path.join('/', install_to))
 
             if count_usr and count_doc:
                 if count_usr == 1:
@@ -1476,10 +1476,10 @@ class PackagingTask(object):
             return False
 
         pkgdocdir = os.path.join(self.packaging.DOCDIR, package.name)
-        dest_pkgdocdir = os.path.join(destdir, pkgdocdir)
+        dest_pkgdocdir = os.path.join(destdir, pkgdocdir.strip('/'))
         mkdir_p(dest_pkgdocdir)
         shutil.copyfile(os.path.join(DATADIR, 'changelog.gz'),
-                os.path.join(destdir, pkgdocdir, 'changelog.gz'))
+                os.path.join(dest_pkgdocdir, 'changelog.gz'))
 
         self.fill_docs(package, destdir, pkgdocdir)
 
@@ -1511,13 +1511,14 @@ class PackagingTask(object):
                 logger.debug('Found %s at %s', wanted.name, copy_from)
 
                 install_to = self.packaging.substitute(package.install_to,
-                        package.name).lstrip('/')
+                        package.name)
 
                 if wanted.install_to is not None:
                     install_to = self.packaging.substitute(wanted.install_to,
                             package.name, install_to=install_to)
 
-                copy_to = os.path.join(destdir, install_to, install_as)
+                copy_to = os.path.join(destdir, install_to.strip('/'), install_as)
+                assert copy_to.startswith(destdir + '/'), (copy_to, destdir)
                 copy_to_dir = os.path.dirname(copy_to)
                 logger.debug('Copying to %s', copy_to)
                 if not os.path.isdir(copy_to_dir):
@@ -1532,11 +1533,11 @@ class PackagingTask(object):
                 else:
                     os.chmod(copy_to, 0o644)
 
-                fullname = os.path.join(install_to, install_as)
+                fullname = os.path.join(install_to, install_as).strip('/')
                 package.md5sums[fullname] = md5
 
         install_to = self.packaging.substitute(package.install_to,
-                package.name).lstrip('/')
+                package.name)
 
         for symlink, real_file in package.symlinks.items():
             symlink = self.packaging.substitute(symlink, package.name,
@@ -1544,8 +1545,8 @@ class PackagingTask(object):
             real_file = self.packaging.substitute(real_file, package.name,
                     install_to=install_to)
 
-            symlink = symlink.lstrip('/')
-            real_file = real_file.lstrip('/')
+            symlink = symlink.strip('/')
+            real_file = real_file.strip('/')
 
             toplevel, rest = symlink.split('/', 1)
             if real_file.startswith(toplevel + '/'):
@@ -1571,7 +1572,8 @@ class PackagingTask(object):
             for i, copy_from in self.cd_tracks[package.name].items():
                 logger.debug('Found CD track %d at %s', i, copy_from)
                 install_as = package.rip_cd['filename_format'] % i
-                copy_to = os.path.join(destdir, install_to, install_as)
+                copy_to = os.path.join(destdir, install_to.strip('/'), install_as)
+                assert copy_to.startswith(destdir + '/'), (copy_to, destdir)
                 copy_to_dir = os.path.dirname(copy_to)
                 if not os.path.isdir(copy_to_dir):
                     mkdir_p(copy_to_dir)
@@ -1906,12 +1908,13 @@ class PackagingTask(object):
                     paths.append(path)
 
             for package in packages:
-                path = '/' + self.packaging.substitute(package.install_to,
-                        package.name).lstrip('/')
+                path = os.path.join('/',
+                        self.packaging.substitute(package.install_to,
+                            package.name).strip('/'))
 
                 if os.path.isdir(path) and path not in paths:
                     paths.append(path)
-                path = '/' + self.packaging.DOCDIR + '/' + package.name
+                path = os.path.join('/', self.packaging.DOCDIR, package.name)
                 if os.path.isdir(path) and path not in paths:
                     paths.append(path)
 

game_data_packager/games/doom_common.py
@@ -145,17 +145,19 @@ class DoomTask(PackagingTask):
             entry['TryExec'] = program
 
             install_to = self.packaging.substitute(package.install_to,
-                    package.name).lstrip('/')
+                    package.name)
 
             if 'args' in quirks:
                 args = quirks['args'] % main_wad
             elif package.expansion_for:
                 iwad = self.game.packages[package.expansion_for].only_file
                 assert iwad is not None, "Couldn't find %s's IWAD" % main_wad
-                args = (  '-iwad /' + install_to + '/' + iwad
-                       + ' -file /' + install_to + '/' + main_wad)
+                iwad = os.path.join('/', install_to, iwad)
+                pwad = os.path.join('/', install_to, main_wad)
+                args = ' '.join(('-iwad', iwad, '-file', pwad))
             else:
-                args = '-iwad /' + install_to + '/' + main_wad
+                iwad = os.path.join('/', install_to, main_wad)
+                args = ' '.join(('-iwad', iwad))
             entry['Exec'] = program + ' ' + args
             entry['Icon'] = desktop_file
             entry['Terminal'] = 'false'

game_data_packager/games/dosbox.py
@@ -62,9 +62,9 @@ class DosboxTask(PackagingTask):
         super(DosboxTask, self).fill_extra_files(package, destdir)
 
         pgm = package.name[:len(package.name)-5]
-        mkdir_p(os.path.join(destdir, self.packaging.BINDIR))
+        mkdir_p(os.path.join(destdir, self.packaging.BINDIR.strip('/')))
         os.symlink('dosgame',
-                   os.path.join(destdir, self.packaging.BINDIR, pgm))
+                   os.path.join(destdir, self.packaging.BINDIR.strip('/'), pgm))
 
         appdir = os.path.join(destdir, 'usr/share/applications')
         mkdir_p(appdir)
@@ -95,8 +95,8 @@ class DosboxTask(PackagingTask):
         entry['Exe'] = package.main_exe
 
         install_to = self.packaging.substitute(package.install_to,
-                       package.name).lstrip('/')
-        with open(os.path.join(destdir, install_to, 'dosgame.inf'),
+                       package.name)
+        with open(os.path.join(destdir, install_to.strip('/'), 'dosgame.inf'),
                   'w', encoding='utf-8') as output:
              dosgame.write(output, space_around_delimiters=False)
 

game_data_packager/games/ecwolf_common.py
@@ -82,7 +82,7 @@ class EcwolfTask(PackagingTask):
             install_data(from_, os.path.join(svgdir, '%s.svgz' % package.name))
 
         install_to = self.packaging.substitute(package.install_to,
-                package.name).lstrip('/')
+                package.name)
 
         desktop = configparser.RawConfigParser()
         desktop.optionxform = lambda option: option
@@ -92,7 +92,7 @@ class EcwolfTask(PackagingTask):
         entry['GenericName'] = self.game.genre + ' game'
         entry['TryExec'] = 'ecwolf'
         entry['Exec'] = 'ecwolf' + package.quirks
-        entry['Path'] = '/' + install_to
+        entry['Path'] = os.path.join('/', install_to)
         entry['Icon'] = package.name
         entry['Terminal'] = 'false'
         entry['Type'] = 'Application'

game_data_packager/games/morrowind.py
@@ -34,9 +34,10 @@ class MorrowindTask(PackagingTask):
             return False
 
         install_to = self.packaging.substitute(package.install_to,
-                                               package.name).lstrip('/')
+                                               package.name)
 
-        datadir = os.path.join(destdir, install_to)
+        datadir = os.path.join(destdir, install_to.strip('/'))
+        assert datadir.startswith(destdir + '/'), (datadir, destdir)
 
         subdir_expected_by_iniimporter = os.path.join(datadir, 'Data Files')
         os.symlink(datadir, subdir_expected_by_iniimporter)
@@ -68,7 +69,7 @@ class MorrowindTask(PackagingTask):
         os.unlink(subdir_expected_by_iniimporter)
 
         with open(cfg, 'a', encoding='utf-8') as f:
-            f.write('data=/%s\n' % install_to)
+            f.write('data=%s\n' % os.path.join('/', install_to))
 
         # then user needs to do this:
         #

game_data_packager/games/residualvm_common.py
@@ -98,7 +98,7 @@ class ResidualvmTask(PackagingTask):
         mkdir_p(appdir)
 
         install_to = self.packaging.substitute(package.install_to,
-                package.name).lstrip('/')
+                package.name)
 
         desktop = configparser.RawConfigParser()
         desktop.optionxform = lambda option: option
@@ -111,7 +111,10 @@ class ResidualvmTask(PackagingTask):
         entry['Terminal'] = 'false'
         entry['Type'] = 'Application'
         entry['Categories'] = 'Game;'
-        entry['Exec'] = 'residualvm -p /%s %s' % (install_to, self.game.gameid)
+        entry['Exec'] = 'residualvm -p %s %s' % (
+                os.path.join('/', install_to),
+                self.game.gameid,
+                )
         with open(os.path.join(appdir, '%s.desktop' % package.name),
                   'w', encoding='utf-8') as output:
              desktop.write(output, space_around_delimiters=False)

game_data_packager/games/scummvm_common.py
@@ -135,9 +135,10 @@ class ScummvmTask(PackagingTask):
         entry['Categories'] = 'Game;%sGame;' % self.game.genre.replace(' ','')
         gameid = package.gameid or self.game.gameid
         install_to = self.packaging.substitute(package.install_to,
-                                               package.name).lstrip('/')
+                                               package.name)
         if len(package.langs) == 1:
-            entry['Exec'] = 'scummvm -p /%s %s' % (install_to, gameid)
+            entry['Exec'] = 'scummvm -p %s %s' % (
+                    os.path.join('/', install_to), gameid)
             self.packaging.override_lintian(destdir, package.name,
                     'desktop-command-not-in-package',
                     'usr/share/applications/%s.desktop %s'
@@ -145,7 +146,8 @@ class ScummvmTask(PackagingTask):
         else:
             pgm = package.name[0:len(package.name)-len('-data')]
             entry['Exec'] = pgm
-            bindir = os.path.join(destdir, self.packaging.BINDIR)
+            bindir = os.path.join(destdir, self.packaging.BINDIR.strip('/'))
+            assert bindir.startswith(destdir + '/'), (bindir, destdir)
             mkdir_p(bindir)
             path = os.path.join(bindir, pgm)
             if 'en' not in package.langs:
@@ -160,9 +162,11 @@ class ScummvmTask(PackagingTask):
                 f.write('done\n')
                 f.write(')\n')
                 f.write('if [ "$GAME_LANG" = "en" ]; then\n')
-                f.write('  exec scummvm -p /%s %s\n' % (install_to, gameid))
+                f.write('  exec scummvm -p %s %s\n' % (
+                    os.path.join('/', install_to), gameid))
                 f.write('else\n')
-                f.write('  exec scummvm -q $GAME_LANG -p /%s %s\n' % (install_to, gameid))
+                f.write('  exec scummvm -q $GAME_LANG -p %s %s\n' % (
+                    os.path.join('/', install_to), gameid))
                 f.write('fi\n')
             os.chmod(path, 0o755)
 

commit 094bbc24b82a4bb43ab707f46e6e7999d0d8207b

make-template: don't crash when unable to guess $install_to
Regression in 0b022b5b, v46.

Bug-Debian: #841164

game_data_packager/make_template.py
@@ -551,7 +551,7 @@ class GameData(object):
                         logger.warning('unhandled data.tar entry type: %s: %s',
                             name, entry.type)
 
-        if self.plugin != 'scummvm_common':
+        if self.plugin != 'scummvm_common' and install_to is not None:
             self.package['install_to'] = os.path.join('/',
                     install_to).replace('/usr/share/games/', '$assets/')
         self.package['install'].sort()